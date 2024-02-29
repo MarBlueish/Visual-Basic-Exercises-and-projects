b8dca61a923071a477d38bdc186e82d76c6d8307
MarBlueish <6958234@formacao.iefp.pt>
2024-02-29 18:01:33 +0000

    changes

--- Ativ9-Projeto/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ativ9_Projeto.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
